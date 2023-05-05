b804e91d128e70a548d65264c3d03fa07c91f01f
Yesika Rodearni <68741697+yesikarodearni@users.noreply.github.com>
2023-05-05 23:03:05 +0700

    Update login.php

--- team6_tk4/login.php
+++ team6_tk4/login.php
@@ -54,25 +54,11 @@ if (isset($_SESSION['id'])) {
 										<input type="password" class="form-control form-control-user"
 											id="exampleInputPassword" placeholder="Password">
 									</div>
-									<div class="form-group">
-										<div class="custom-control custom-checkbox small">
-											<input type="checkbox" class="custom-control-input" id="customCheck">
-											<label class="custom-control-label" for="customCheck">Remember
-												Me</label>
-										</div>
-									</div>
 									<a href="index.html" class="btn btn-primary btn-user btn-block">
 										Login
 									</a>
-									<hr>
 								</form>
-								<hr>
-								<div class="text-center">
-									<a class="small" href="forgot-password.html">Forgot Password?</a>
-								</div>
-								<div class="text-center">
-									<a class="small" href="register.html">Create an Account!</a>
-								</div>
+								
 							</div>
 						</div>
 					</div>
