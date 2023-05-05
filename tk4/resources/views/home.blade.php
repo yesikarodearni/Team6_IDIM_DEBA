1d6966447cf92f07fb889fdce6b528864c610d72
Yesika Rodearni <68741697+yesikarodearni@users.noreply.github.com>
2023-05-05 10:22:15 +0700

    Update home.blade.php

--- tk4/resources/views/home.blade.php
+++ tk4/resources/views/home.blade.php
@@ -3,23 +3,27 @@
 @section('main-content')
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">{{ __('Dashboard') }}</h1>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-4 text-gray-800">{{ __('Dashboard') }}</h1>
 
-    @if (session('success'))
-    <div class="alert alert-success border-left-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    @endif
-
-    @if (session('status'))
-        <div class="alert alert-success border-left-success" role="alert">
-            {{ session('status') }}
+        @if (session('success'))
+        <div class="alert alert-success border-left-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
-    @endif
+        
+        @endif
 
+        @if (session('status'))
+            <div class="alert alert-success border-left-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+            class="fas fa-download fa-sm text-white-50"></i> Laporan Rugi Laba </a>
+    </div>
     <div class="row">
 
         <!-- Earnings (Monthly) Card Example -->
